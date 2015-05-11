0e007f50b073d2b9971307efff3550f77325d9c1
Ian Wood <theonlyianwood@gmail.com>
2015-05-11 17:35:30 +0100

    Chapter 03: Master: Now does the runForMaster stuff - to page 55 or so.

--- src/main/scala/chapter_03/Master.scala
+++ src/main/scala/chapter_03/Master.scala
@@ -1,7 +1,11 @@
 package chapter_03
 
 
-import org.apache.zookeeper.{Watcher, ZooKeeper, WatchedEvent}
+import org.apache.zookeeper.KeeperException.{NoNodeException, NodeExistsException}
+import org.apache.zookeeper._
+import org.apache.zookeeper.data.Stat
+
+import scala.util.Random
 
 /**
  * use:
@@ -12,11 +16,20 @@ import org.apache.zookeeper.{Watcher, ZooKeeper, WatchedEvent}
  */
 object Master {
 
-  def main(args: Array[String]): Unit ={
-   val m = new Master(args(0))
+  def main(args: Array[String]): Unit = {
+    val m = new Master(args(0))
     m.startZk()
 
-    Thread.sleep(60000)
+    if (m.runForMaster()) {
+      println("I am the leader")
+      Thread.sleep(60000)
+    } else {
+      println("someone else is the leader")
+    }
+
+
+
+    m.stopZk()
   }
 
 }
@@ -26,14 +39,57 @@ object Master {
  */
 class Master(hostPort: String) extends Watcher {
 
+  //  var isLeader = false
+  val r = new Random()
+  val serverId = Integer.toHexString(r.nextInt()).getBytes()
+
   var zk: ZooKeeper = null
 
   def startZk(): Unit = {
     zk = new ZooKeeper(hostPort, 15000, this)
   }
 
+  def checkForMaster(): Boolean = {
+    while (true) {
+      try {
+        val stat = new Stat
+        val data = zk.getData("/master", false, stat)
+        new String(data).equals(serverId)
+      } catch {
+        case e: NoNodeException => false
+      }
+    }
+    false
+  }
+
+  def runForMaster(): Boolean = {
+    var continue = true
+    while (continue) {
+      try {
+        zk.create("/master", serverId, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL)
+        continue = false
+        return true
+      } catch {
+        case e: NodeExistsException => {
+          println(s"xxx NodeExistsException $e")
+          return false
+        }
+        case e: InterruptedException => {
+
+          println(s"xxx InterruptedException $e")
+        }
+      }
+    }
+
+    checkForMaster()
+
+  }
+
   override def process(event: WatchedEvent): Unit = println(s"xxxxx [$event].")
 
+  def stopZk(): Unit = {
+    zk.close()
+  }
 
 
 }
