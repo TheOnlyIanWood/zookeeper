a5451b7b6b7cbe24580502684fa52c44aae077e3
Ian Wood <theonlyianwood@gmail.com>
2015-05-12 14:15:19 +0100

    Chapter 03: MasterAsync: got the Async working, before the main was calling the non async version, ffs.                         Got the aysnc bootstrapping working too.

--- src/main/scala/chapter_03/MasterAsync.scala
+++ src/main/scala/chapter_03/MasterAsync.scala
@@ -4,28 +4,28 @@ import org.apache.zookeeper.AsyncCallback.{DataCallback, StringCallback}
 import org.apache.zookeeper.KeeperException.{NoNodeException, Code}
 import org.apache.zookeeper._
 import org.apache.zookeeper.data.Stat
-
 import scala.util.Random
+import org.slf4j.LoggerFactory
+
+trait Logger {
+  val log = LoggerFactory.getLogger(this.getClass)
+
+}
 
 object MasterAsync {
 
 
   def main(args: Array[String]): Unit = {
-    val m = new Master(args(0))
+    val m = new MasterAsync(args(0))
     m.startZk()
-
-    if (m.runForMaster()) {
-      println("I am the leader")
-      Thread.sleep(60000)
-    } else {
-      println("Someone else is the leader")
-    }
+    m.runForMaster()
+    Thread.sleep(60000)
 
     m.stopZk()
   }
 }
 
-class MasterAsync(hostPort: String) extends Watcher {
+class MasterAsync(hostPort: String) extends Watcher with Logger {
 
   var isLeader = false
 
@@ -36,18 +36,20 @@ class MasterAsync(hostPort: String) extends Watcher {
   private lazy val zk: ZooKeeper = new ZooKeeper(hostPort, 15000, this)
 
   def startZk(): Unit = {
-    println(s"startZk [$zk]")
+    log.info(s"startZk [$zk]")
   }
 
   def masterCreateCallBack = new StringCallback {
     override def processResult(rc: Int, path: String, ctx: scala.Any, name: String): Unit = {
       Code.get(rc) match {
         case Code.CONNECTIONLOSS => checkMaster()
-        case Code.OK => isLeader = true
+        case Code.OK =>
+          isLeader = true
+          bootstrap()
         case _ => isLeader = false
       }
 
-      println(s"I am ${if (isLeader) "" else " not "} the leader")
+      log.info(s"I am ${if (isLeader) "" else " not "}the leader")
     }
   }
 
@@ -72,4 +74,32 @@ class MasterAsync(hostPort: String) extends Watcher {
   }
 
   override def process(event: WatchedEvent): Unit = println(s"xxxxx [$event].")
+
+  def bootstrap(): Unit = {
+    log.info("bootstrap")
+    createParent("/workers")
+    createParent("/assign")
+    createParent("/tasks")
+    createParent("/status")
+  }
+
+  def createParent(path: String, data: Array[Byte] = Array[Byte]()): Unit = {
+    log.info(s"Creating parent [$path]")
+    zk.create(path, data, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT, createParentCallback, data)
+  }
+
+  def createParentCallback = new StringCallback {
+    override def processResult(rc: Int, path: String, ctx: scala.Any, name: String): Unit = {
+      Code.get(rc) match {
+        case Code.CONNECTIONLOSS => createParent(path, ctx.asInstanceOf[Array[Byte]])
+        case Code.OK => log.info(s"Parent created [$path].")
+        case Code.NODEEXISTS => log.warn(s"Parent already exists [$path].")
+      }
+    }
+  }
+
+  def stopZk(): Unit = {
+    zk.close()
+  }
+
 }
